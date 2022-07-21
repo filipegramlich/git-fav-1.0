2193b3e0eac20442d35b7d6a6a7e261c449966a6
filipe <filipe.ogramlich@gmail.com>
2022-07-20 23:23:51 -0300

    essential-classes/html/css-created

---
+++ js/favorites.js
@@ -0,0 +1,119 @@
+export class GitHubUser {
+    static search(username){
+        const endpoint = `https://api.github.com/users/${username}`;
+
+        return fetch(endpoint).then(data => data.json())
+            .then(({login, name, public_repos, followers})=>(
+                {
+
+                    login,
+                    name,
+                    public_repos,
+                    followers
+
+                }
+            )
+        )
+    } 
+}
+
+export class Favorites {
+    constructor(root){
+
+        this.root = document.querySelector(root);
+
+        this.load();
+        this.tbody = this.root.querySelector("table tbody");
+
+    }
+
+    load(){
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || [];
+    }
+
+    async add(username){
+        const user = await GitHubUser.search(username);
+    }
+
+    delete(user){
+        const filteredEntries = this.entries.filter(entry =>entry.login !== user.login);
+        this.entries = filteredEntries;
+        this.update();
+    }
+}
+
+export class FavoritesView extends Favorites{
+    constructor (root){
+
+        super(root);
+
+        this.update();
+        this.onAddButton();
+
+    }
+
+    createRow(){
+        const tr = document.createElement("tr");
+        tr.innerHTML = `
+            <td class="user">
+                <img src="https://github.com/ldantascruz.png" alt="Imagem do Lucas">
+                <a href="https://github.com/ldantascruz" target="_blank">
+                    <p>Lucas Cruz</p>
+                    <span>ldantascruz</span>
+                </a>
+            </td>
+            <td class="repositories">
+                18
+            </td>
+            <td class="followers">
+                40000
+            </td>
+            <td>
+                <button class ="remove">&times;</button>
+            </td> `
+
+        return tr;
+    }
+
+    update(){
+        this.removeAllRows();
+        this.entries
+            .forEach(user=>{
+                const row = this.createRow();
+
+                row.querySelector(".user img").src = `https://github.com/${user.login}.png`
+                row.querySelector(".user p").textContent = user.name;
+                row.querySelector(".user span").textContent = user.login;
+                row.querySelector(".repositories").textContent = user.public_repos;
+                row.querySelector(".followers").textContent = user.followers;
+                row.querySelector(".remove")
+                    .onclick = ()=>{
+
+                            const isOkay = confirm("Tem certeza que quer deletar?");
+                            if(isOkay)
+                            {
+                                this.delete(user);   
+                    }
+                }
+                this.tbody.append(row);
+            }
+        )
+    }
+
+    onAddButton(){
+        const addButton = this.root.querySelector(".search button")
+        addButton.onclick 
+            =()=>{
+
+            const {value} = this.root.querySelector(".search input")
+            this.add(value);
+
+        }
+    }
+    removeAllRows(){
+            this.tbody.querySelectorAll("tr").forEach((tr)=>{
+                tr.remove();
+            }
+        )
+    }
+}
